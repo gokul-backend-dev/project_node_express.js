c78b29cdd7d6923b245d6f02e7b745274d2f03fe
gokul-backend-dev <gokulnathking40@gmail.com>
2025-07-08 22:09:29 +0530

    router chage

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.listen(5001, () => console.log("Api running on port 5001"))
 
-app.get("/hi", (req, res) => res.json('My Api running---->'))
+app.get("/", (req, res) => res.json('My Api running---->'))
